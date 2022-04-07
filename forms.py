371933ba1e60d3884a6e077055b764ae0b9717bf
hussain <orientsplash@gmail.com>
2022-04-07 15:57:57 +0530

    trying to get it to work2

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import DataRequired, URL, Email, Length
+from wtforms.validators import DataRequired, URL, Length
 from flask_ckeditor import CKEditorField
 
 
@@ -15,13 +15,13 @@ class CreatePostForm(FlaskForm):
 
 class RegistrationForm(FlaskForm):
     username = StringField("Your name", validators=[DataRequired()])
-    email = StringField("Email", validators=[DataRequired(), Email("Enter a valid Email")])
+    email = StringField("Email", validators=[DataRequired()])
     password = PasswordField("Password", validators=[DataRequired(), Length(min=10)])
     submit = SubmitField("Register")
 
 
 class LoginForm(FlaskForm):
-    email = StringField("Email", validators=[DataRequired(), Email("Enter a valid Email")])
+    email = StringField("Email", validators=[DataRequired()])
     password = PasswordField("Password", validators=[DataRequired(), Length(min=10)])
     submit = SubmitField("Log In")
 
